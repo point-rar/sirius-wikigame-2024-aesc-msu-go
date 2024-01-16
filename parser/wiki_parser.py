e22cd234a71eb45c84c70b280945ddf012835d6d
Anton <qualdoomoscow@gmail.com>
2024-01-16 15:23:20 +0300

    Fixed wikiparser (make it faster and add backlinks)

--- parser/wiki_parser.py
+++ parser/wiki_parser.py
@@ -50,3 +50,84 @@ class WikiParserDumb(WikiParser):
                 links.add(Link(title))
 
         return links
+
+
+class WikiParserSmarter(WikiParser):
+    def __init__(self):
+        self.session = requests.Session()
+
+    def __get_backlinks_from_page(self, page_name: str, count_backlinks=500) -> str:
+        # See: https://en.wikipedia.org/w/api.php?action=help&modules=parse
+        # https://en.wikipedia.org/w/api.php, https://ru.wikipedia.org/w/api.php
+        params_query = {
+            'action': 'query',
+            'bltitle': page_name,
+            'format': 'json',
+            'list': 'backlinks',
+            'bllimit':  count_backlinks
+        }
+
+        req = self.session.get(url=self.URL, params=params_query)
+        data = req.json()
+
+        # print(data.keys())
+        #
+        # print(data['query'].keys())
+        # print(data['query']['backlinks'])
+
+        return data['query']['backlinks']
+    def __get_links_from_page(self, page_name: str) -> str:
+        # See: https://en.wikipedia.org/w/api.php?action=help&modules=parse
+        # https://en.wikipedia.org/w/api.php, https://ru.wikipedia.org/w/api.php
+
+        params_parse = {
+            'action': 'parse',
+            'page': page_name,
+            'format': 'json',
+            'props': 'links'
+        }
+
+        req = self.session.get(url=self.URL, params=params_parse)
+        data = req.json()
+
+        # print(data.keys())
+        #
+        # print(data['parse'].keys())
+        # print(data['parse']['links'])
+
+        return data['parse']['links']
+
+    def get_links(self, page_name: str) -> set[Link]:
+        logger.info(f"Parsing links from '{page_name}'")
+        raw_links = self.__get_links_from_page(page_name)
+
+        links = set()
+
+        for raw_link in raw_links:
+            title = raw_link['*'] # like a name
+            # Avoid beginnings
+            # Help: Wikipedia: Special: Category: Template: User talk:
+            if ':' not in title:
+                links.add(Link(title))
+
+        return links
+
+    def get_backlinks(self, page_name: str) -> set[Link]:
+        logger.info(f"Parsing backlinks from '{page_name}'")
+        raw_links = self.__get_backlinks_from_page(page_name)
+
+        links = set()
+
+        for raw_link in raw_links:
+            title = raw_link['title'] # like a name
+            # Avoid beginnings
+            # Help: Wikipedia: Special: Category: Template: User talk:
+            if ':' not in title:
+                links.add(Link(title))
+
+        return links
+
+
+# wiki = WikiParserSmarter()
+
+# print(wiki.get_backlinks("cat"))

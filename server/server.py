108da8ddaa3030dd8552b71e235e8d3fc20309e4
FynjyBath <anton5-206@mail.ru>
2024-01-17 11:22:45 +0300

    PEP8

--- server/server.py
+++ server/server.py
@@ -1,34 +1,31 @@
-# from gensim.models import Word2Vec, KeyedVectors
-# from gensim.test.utils import common_texts
-# from gensim.scripts.glove2word2vec import glove2word2vec
-# from gensim.test.utils import datapath, get_tmpfile
-# import gensim
-# from gensim import downloader
-#
-# # glove_file = datapath('glove.6B.300d.txt')
-#
-# model = gensim.downloader.load('fasttext-wiki-news-subwords-300')
-#
-# from flask import Flask, request
-#
-# app = Flask(__name__)
-#
-# import json
-#
-#
-# @app.post('/')
-# def get_query():
-#     links = request.get_json()['links']
-#     who = request.get_json()['word']
-#
-#     result = []
-#
-#     for i in range(len(links)):
-#         try:
-#             result.append(str(model.similarity(links[i], who)))
-#         except:
-#             result.append(str(-1))
-#     return json.dumps({"result": result}), 200
-#
-#
-# app.run()
+from gensim.models import Word2Vec, KeyedVectors
+from gensim.test.utils import common_texts
+from gensim.scripts.glove2word2vec import glove2word2vec
+from gensim.test.utils import datapath, get_tmpfile
+import gensim
+from gensim import downloader
+from flask import Flask, request
+import json
+
+# glove_file = datapath('glove.6B.300d.txt')
+
+model = gensim.downloader.load('fasttext-wiki-news-subwords-300')
+
+app = Flask(__name__)
+
+@app.post('/')
+def get_query():
+    links = request.get_json()['links']
+    who = request.get_json()['word']
+
+    result = []
+
+    for i in range(len(links)):
+        try:
+            result.append(str(model.similarity(links[i], who)))
+        except:
+            result.append(str(-1))
+    return json.dumps({"result": result}), 200
+
+
+app.run()

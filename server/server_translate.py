667e4bc67e449b9c298e561f2221503da3150394
FynjyBath <anton5-206@mail.ru>
2024-01-18 15:12:51 +0300

    add map to translate

--- server/server_translate.py
+++ server/server_translate.py
@@ -5,12 +5,22 @@ from googletrans import Translator
 
 translator = Translator()
 app = Flask(__name__)
+mp = {}
 
 
 @app.post('/')
 def get_query():
     words = request.get_json()['words']
-    result = translator.translate('. '.join(words), src='ru', dest='en').text.split('.')
+    inds = []
+    result = [''] * len(words)
+    for i in range(len(words)):
+        if words[i] in mp.keys():
+            result[i] = mp[words[i]]
+            continue
+        inds.append(i)
+    q_result = translator.translate('. '.join([words[i] for i in inds]), src='ru', dest='en').text.split('.')
+    for i in range(len(inds)):
+        mp[words[inds[i]]] = result[inds[i]] = q_result[i]
     return json.dumps({"result": result}), 200
 
 
